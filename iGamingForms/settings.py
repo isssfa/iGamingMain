5641b1056c0d87e86a3ddd6fa76e6f171c4c0428
ibrahim <ibrahimkhan02016@gmail.com>
2025-06-25 21:15:57 -0500

    correction

--- iGamingForms/settings.py
+++ iGamingForms/settings.py
@@ -131,17 +131,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-
 STATIC_URL = '/static/'
+
+# Add directories to look for static files (optional in development)
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static")
+    BASE_DIR / "static",
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 # Path where media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
