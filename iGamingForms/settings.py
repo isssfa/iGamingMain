2f64aaaf12fa4487569dcc7c1207b06de093bac6
ibrahim <ibrahimkhan02016@gmail.com>
2025-06-25 20:58:21 -0500

    sites

--- iGamingForms/settings.py
+++ iGamingForms/settings.py
@@ -136,6 +136,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static")
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 MEDIA_URL = '/media/'
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
