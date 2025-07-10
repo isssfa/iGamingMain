5e467892a9bdcc736e2c6c53b81762b27b5072fc
ibrahim <ibrahimkhan02016@gmail.com>
2025-07-10 02:42:19 -0500

    updated

--- iGamingForms/api_urls.py
+++ iGamingForms/api_urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('base/', include('base.urls')),
+    path('', include('base.urls')),
     path('sponsor/', include('sponsor.urls')),
     path('sponsorship/', include('sponsorship.urls')),
     path('exhibition/', include('exhibition.urls')),
