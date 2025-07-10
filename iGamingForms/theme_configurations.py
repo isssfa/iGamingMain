cec466d74620669fd6ecf945baa5c3128b0ba456
ibrahim <ibrahimkhan02016@gmail.com>
2025-07-10 02:33:59 -0500

    updated

--- iGamingForms/theme_configurations.py
+++ iGamingForms/theme_configurations.py
@@ -53,7 +53,7 @@ JAZZMIN_SETTINGS = {
         {"model": "auth.User"},
 
         # App with dropdown menu to all its models pages (Permissions checked against models)
-        {"app": "books"},
+        {"app": "base"},
     ],
 
     #############
